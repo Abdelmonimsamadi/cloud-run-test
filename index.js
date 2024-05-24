8a13891292e325e3e2ce26feb36724f4b25201cc
MrAbdul <monimesamadi@gmail.com>
2024-05-24 15:09:30 +0100

    feat: added mode env

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+const MODE = process.env.MODE;
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World test github actions</h1>");
+  res.send(`<h1>Hello World from mode: ${MODE}</h1>`);
 });
 
 app.listen(PORT, () => {
